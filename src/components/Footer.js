73ec1b83d9278ba7d48abd44f4f5c534be5d475c
Gnilane95 <agathegnilanediouf@gmail.com>
2022-06-30 16:53:12 +0200

    Footer integrated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-grayfooter">
-      <div className="grid grid-cols-4">
-        <div className="col-span-4">cljidgjkx</div>
-        
-        <div className="col-span-2">llhksqgx</div>
-        
-        <div className="col-span-2">lcilds</div>
+    <div className="bg-grayfooter py-40 px-36">
+      <div className="grid grid-cols-4 ">
+        <div className="">
+          <img src="img/logo.png" alt="" />
+          <p className=''>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi error perspiciatis commodi ea possimus
+          </p>
+        </div>
 
-        <div className="col-span-4">dlikjgcbx</div>
+        <div className="">llhksqgx</div>
+
+        <div className="">lcilds</div>
+
+        <div className="">dlikjgcbx</div>
       </div>
     </div>
   );
